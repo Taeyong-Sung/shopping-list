c8226027bd74c5bec8837e36f67d5f7f0bdfb22d
Taeyong Sung <canny0201@gmail.com>
2024-08-12 14:06:01 -0400

    create sample list, shopping list, and edit page link

--- controllers/items.js
+++ controllers/items.js
@@ -2,13 +2,35 @@ import { Item } from "../models/item.js"
 
 async function index(req, res) {
     try {
-      res.render('items/index')
+        res.render('items/index')
     } catch (error) {
-      console.log(error)
-      res.redirect('/')
+        console.log(error)
+        res.redirect('/')
     }
   }
 
+async function show(req,res) {
+    try {
+        res.render('items/show')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+}
+
+async function edit(req, res) {
+    try {
+      res.render('items/edit', {
+      })
+    } catch (error) {
+      console.log(error)
+      res.redirect('/items')
+    }
+  }
+
+
 export {
-    index
+    index,
+    show,
+    edit
   }
--- routes/items.js
+++ routes/items.js
@@ -4,8 +4,15 @@ import * as itemsCtrl from '../controllers/items.js'
 
 const router = Router()
 
+
+// public routes
+//items/index
 router.get('/', itemsCtrl.index)
 
 
+// protected routes //
+//items/show
+router.get('/show', isSignedIn, itemsCtrl.show)
+router.get('/edit', isSignedIn, itemsCtrl.edit)
 
 export { router }
