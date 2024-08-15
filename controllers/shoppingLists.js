ffff04ad52ce5a6f7d9806cb393628bd72084bdf
Taeyong Sung <canny0201@gmail.com>
2024-08-15 15:14:47 -0400

    add no duplicats in my shopping list/items/update

--- controllers/shoppingLists.js
+++ controllers/shoppingLists.js
@@ -40,8 +40,14 @@ async function newItems(req, res) {
 async function create(req, res){
     try {
         req.body.owner = req.session.user._id
+        const userOwner = await User.findById(req.body.owner).populate('shoppingLists')
+        const shoppingListExists = userOwner.shoppingLists.some(shoppingList => 
+            shoppingList.name.toLowerCase() === req.body.name.toLowerCase()
+        )
+        if (shoppingListExists) {
+            return res.send('Shopping List of same name already exists.')
+        }
         const shoppingList = await ShoppingList.create(req.body)
-        const userOwner = await User.findById(req.body.owner)
         userOwner.shoppingLists.push(shoppingList)
         await userOwner.save()
         res.redirect(`/shoppingLists/${shoppingList._id}`)
@@ -53,7 +59,13 @@ async function create(req, res){
 
 async function createItems(req, res){
     try {
-        const shoppingList = await ShoppingList.findById(req.params.shoppingListId)
+        const shoppingList = await ShoppingList.findById(req.params.shoppingListId).populate('items')
+        const itemExists = shoppingList.items.some(item => 
+            item.name.toLowerCase() === req.body.name.toLowerCase()
+        )
+        if (itemExists) {
+            return res.send('Item of same name already exists.')
+        }
         shoppingList.items.push(req.body)
         await shoppingList.save()
         res.redirect(`/shoppingLists/${shoppingList._id}/new`)
@@ -80,7 +92,13 @@ async function edit(req, res) {
 
 async function update(req, res) {
     try {
-        const shoppingList = await ShoppingList.findById(req.params.shoppingListId)
+        const shoppingList = await ShoppingList.findById(req.params.shoppingListId).populate('items')
+        const itemExists = shoppingList.items.some(item => { 
+            return item.name.toLowerCase() === req.body.name.toLowerCase()
+        })
+        if (itemExists) {
+            return res.send('Item of same name already exists.')
+        }
         const item = shoppingList.items.id(req.params.itemId)
         item.set(req.body)
         await shoppingList.save()
@@ -105,8 +123,10 @@ async function deleteItem(req, res) {
 
 async function deleteList(req, res) {
     try {
-        await ShoppingList.findByIdAndDelete(req.params.shoppingListId)
+        const shoppingList = await ShoppingList.findById(req.params.shoppingListId)
         const user = await User.findById(req.session.user._id)
+        user.shoppingLists.remove(shoppingList)
+        await user.save()
         res.redirect(`/users/${user._id}`);
     } catch (error) {
         console.log(error)
